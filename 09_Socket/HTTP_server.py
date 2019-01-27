2581896df058e68cf2097ed6dd404ea310c63fd0
Jan Matecha <jan.matecha@gmail.com>
2019-01-27 16:19:46 +0100

    http server - nefunguje

--- 09_Socket/HTTP_server.py
+++ 09_Socket/HTTP_server.py
@@ -9,7 +9,7 @@ html = """<!DOCTYPE html>
 """
 # <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
 
-addr = socket.getaddrinfo('127.0.0.1', 80)[0][-1]
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 
 s = socket.socket()
@@ -22,15 +22,19 @@ while True:
     cl, addr = s.accept()
     print('client connected from', addr)
     cl_file = cl.makefile('rwb', 0)
+    print("cl_file", cl_file)
     while True:
         line = cl_file.readline()
+        print("line",line)
         if not line or line == b'\r\n':
             break
+
     # rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
     # response = html % '\n'.join(rows)
     # response = html % "ABC"
     print("tady")
-    cl.send(html.encode('utf-8'))
+    # cl.send(html.encode('utf-8'))
+    cl.send("HTTP/1.0 200 OK\r\n\r\n<h1>Hallo Michael, how are you?</h1>\r\n".encode('utf-8'))
     print("tady2")
     cl.close()
 
